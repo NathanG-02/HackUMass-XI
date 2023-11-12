605a2ff2b372beae03df5e05c6a0bf8205f1adb9
aelithaz <anzhang@umass.edu>
2023-11-12 04:12:33 -0500

    Connected gameOverPage back to HomePage

--- lib/gameOverPage.dart
+++ lib/gameOverPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:umass_geoguessr_app/gamePage.dart';
+import 'package:umass_geoguessr_app/homePage.dart';
 
 class GameOverPage extends StatefulWidget {
   final int score;
@@ -13,35 +14,42 @@ class GameOverPage extends StatefulWidget {
 class _GameOverPageState extends State<GameOverPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Score: ${widget.score}")),
-        Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Time: ${widget.time}")),
-        Container(
-            padding: const EdgeInsets.all(8.0),
-            child: const Text("Play again?")),
-        Row(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
+    return Container(
+      decoration: BoxDecoration(color: const Color.fromARGB(255, 163, 25, 25)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [ 
+          Text("Score: ${widget.score}", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, decoration: TextDecoration.none)),
+          Text("Time: ${widget.time}", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, decoration: TextDecoration.none)),
+          Text("Play again?", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, decoration: TextDecoration.none)),
+          Container(
+            padding: EdgeInsets.all(30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 76, 76, 76),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
                       builder: (BuildContext context) => const GamePage()));
-                },
-                child: const Text("Yes")),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
+                  },
+                  child: const Text("Yes", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 76, 76, 76),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
                     // link to home page
-                      builder: (BuildContext context) => const GamePage()));
-                },
-                child: const Text("No")),
-          ],
-        ),
-      ],
-    );
+                      builder: (BuildContext context) => const HomePage()));
+                  },
+                  child: const Text("No", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white))),
+              ],
+            ),
+          ),
+        ],
+    ));
   }
 }
